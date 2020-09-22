6aa1ec6bb8fa82c280b935f4d748dc76ec18d612
陈立林 <lilin.chen@brilliant-solution.com>
2020-09-22 18:29:20 +0800

    添加单元测试，添加代理模式判断是否允许用户外部改变。

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,14 @@ interface Sheet {
 }
 
 interface Init {
-  dataBase: { [prop: string]: object };
-  changeable: boolean; //是否允许外部改变内部
-  persist: boolean;
+  dataBase: Record<string, object>;
+  changeable?: boolean; //是否允许外部改变内部
+  persist?: boolean;
   name?: string;
 }
 
 function hasKey(key: strOrArr | undefined): boolean {
-  if (Boolean(key)) {
+  if (key !== undefined) {
     return true;
   } else {
     throw new Error('The key value is invalid');
@@ -24,24 +24,28 @@ function hasKey(key: strOrArr | undefined): boolean {
 }
 
 class Dict {
-  private dataBase: { [prop: string]: object } = {};
+  private dataBase: any = {};
   private changeable: boolean = false; //是否允许外部改变内部
-  private persist: boolean = true;
-  private name;
+  // private persist: boolean = true;
+  // private name:string = "";
+  private changeFlag = false; // 是否进行内部改变
 
   // 待代理set优化
   constructor(option?: Init) {
-    if (option !== undefined) {
-      this.init(option);
-    }
+    // if (option !== undefined) {
+    // }
+    this.init(option);
   }
 
-  public init(option: Init): object {
-    this.dataBase = option.dataBase;
-    this.changeable = option.changeable;
-    this.persist = option.persist;
-    this.name = option.name;
-
+  public init(option?: Init): object {
+    let def: Init = {
+      dataBase: {},
+      changeable: false,
+      persist: true,
+      name: '',
+    };
+    Object.assign(this, def, option);
+    this.dataBase = this.watch(this.dataBase);
     return this.dataBase;
   }
 
@@ -49,11 +53,30 @@ class Dict {
     return this.dataBase;
   }
 
+  public watch(obj: object | any[]) {
+    return new Proxy(obj, {
+      get: (target, key) => {
+        return target[key];
+      },
+      set: (target, key, value) => {
+        let falg = this.changeable || this.changeFlag;
+        if (falg) {
+          target[key] = value;
+        } else {
+          throw new Error('Dict is not changeable.');
+        }
+
+        this.changeFlag = false;
+        return falg;
+      },
+    });
+  }
+
   public addSheet(sheetObj: Sheet): object {
     let { name, data, key } = sheetObj;
-
     let sheet = {};
 
+    this.changeFlag = true;
     if (Array.isArray(data)) {
       hasKey(key);
       data.map(row => {
@@ -65,9 +88,9 @@ class Dict {
           });
           keys = arr.join('_');
         } else {
-          keys = key;
+          keys = row[String(key)];
         }
-        sheet[String(keys)] = row;
+        sheet[keys] = row;
       });
     } else {
       sheet = data;
@@ -81,6 +104,7 @@ class Dict {
   }
 
   public delSheet(name: string): boolean {
+    this.changeFlag = true;
     if (Boolean(this.dataBase[name])) {
       delete this.dataBase[name];
       return true;
@@ -90,7 +114,7 @@ class Dict {
   }
 
   public hasSheet(name: string) {
-    return Boolean(this.dataBase[name]);
+    return name in this.dataBase;
   }
 
   public getItem(name: string, key: string): any {
@@ -99,6 +123,7 @@ class Dict {
   }
 
   public delItem(name: string, key: string): boolean {
+    this.changeFlag = true;
     let sheet = this.dataBase[name];
 
     if (key in sheet) {
